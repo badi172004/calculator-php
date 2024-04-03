437eb14ec5a55b319a18738520b3f2757e8b2b47
KRISTIANNE <badigaming172004@gmail.com>
2024-04-04 06:11:33 +0800

    commit

--- index.php
+++ index.php
@@ -1,32 +1,155 @@
+<?php
+    $cookie_name1="num";
+    $cookie_value1="";
+    $cookie_name2="op";
+    $cookie_value2="";
+
+    if(isset($_POST['num']))
+    {
+     $num=$_POST['input'].$_POST['num'] ;
+    }
+    else{
+        $num="";
+    }
+    if(isset($_POST['op']))
+    {
+        $cookie_value1=$_POST['input'];
+        setcookie($cookie_name1, $cookie_value1, time()+(86400*30), "/");
+
+        $cookie_value2=$_POST['op'];
+        setcookie($cookie_name2, $cookie_value2, time()+(86400*30), "/");
+        $num="";
+    }
+    if(isset($_POST['equal']))
+    {
+        $num=$_POST['input'];
+        switch($_COOKIE['op'])
+        {
+            case "+":
+                $result=$_COOKIE['num']+$num;
+                break;
+                case "-":
+                    $result=$_COOKIE['num']-$num;
+                    break;
+                    case "*":
+                        $result=$_COOKIE['num']*$num;
+                        break;
+                        case "/":
+                            $result=$_COOKIE['num']/$num;
+                            break;
+        }
+        $num=$result;
+    }
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CrossOver Calculator</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Calculator</title>
+    <style>
+        body{
+            background-color: rgb(163, 159, 159);
+        }
+       
+        .calc{
+            
+            margin: auto;
+            background-color: black;
+            border: 2px solid whitesmoke;
+            width: 24%;
+            height: 630px;
+            border-radius: 20px;
+            box-shadow: 10px 10px 40px;
+        }
+        .maininput{
+            background-color: black;
+            border: 1px solid grey;
+            height: 125px;
+            width: 98.2%;
+            font-size: 80px;
+            color: whitesmoke;
+            font-weight: 00;
+        }
+        .numbtn{
+            padding: 30px 35px;
+            border-radius: 50px;
+            font-weight: 500;
+            font-size: large;
+            background-color: rgb(155, 154, 154);
+        }
+        .numbtn:hover{
+            background-color: rgb(136, 133, 133);
+            color: whitesmoke;
+        }
+        .calbtn{
+            padding: 30px 35px;
+            border-radius: 50px;
+            font-weight: 500;
+            font-size: 30px;
+            background-color: orange;
+        }
+        .calbtn:hover{
+            background-color: rgb(211, 140, 7);
+            color: whitesmoke;
+        }
+        .c{
+            padding: 30px 35px;
+            border-radius: 50px;
+            font-weight: 500;
+            font-size: large;
+            background-color: red;
+        }
+        .c:hover{
+            background-color: rgb(188, 16, 16);
+            color: whitesmoke;
+        }
+        .equal{
+            padding: 30px 35px;
+            border-radius: 50px;
+            font-weight: 500;
+            font-size: large;
+            background-color: rgb(8, 181, 8);
+        }
+        .equal:hover{
+            background-color: green;
+            color: whitesmoke;
+        }
+
+    </style>
 </head>
 <body>
-    <h1>Calculator</h1>
-    <form action="" name="clc">
-        <input type="text" name="display" style="width: 370px; height: 70px; background-color: #4c555c;color: white;"><br>
-        <input class="btn" type="button" value="0" onclick="clc.display.value += '0'">
-        <input class="btn" type="button" value="1" onclick="clc.display.value += '1'">
-        <input class="btn" type="button" value="2" onclick="clc.display.value += '2'">
-        <input class="btn" type="button" value="+" onclick="clc.display.value += '+'" style=" background-color: #cc5c11;"><br>
-        <input class="btn" type="button" value="3" onclick="clc.display.value += '3'">
-        <input class="btn" type="button" value="4" onclick="clc.display.value += '4'">
-        <input class="btn" type="button" value="5" onclick="clc.display.value += '5'">
-        <input class="btn" type="button" value="-" onclick="clc.display.value += '-'" style="background-color: #ba55d3;"><br>
-        <input class="btn" type="button" value="6" onclick="clc.display.value += '6'">
-        <input class="btn" type="button" value="7" onclick="clc.display.value += '7'">
-        <input class="btn" type="button" value="8" onclick="clc.display.value += '8'">
-        <input class="btn" type="button" value="*" onclick="clc.display.value += '*'" style="background-color: #7db1b2;"><br>
-        <input class="btn" type="button" value="9" onclick="clc.display.value += '9'">
-        <input class="btn" type="button" value="C" onclick="clc.display.value =''" style="background-color:#cc0000;">
-        <input class="btn" type="button" value="=" onclick="clc.display.value =eval(clc.display.value)">
-        <input class="btn" type="button" value="&#247;" onclick="clc.display.value += '/'" style="background-color: #1f80c9;">
+        <div class="calc">
+            <form action="" method="post">
+                <br>
+                <input type="text" class="maininput" name="input" value="<?php echo @$num ?>"> <br> <br>
+                <input type="submit" class="numbtn" name="num"value="7">
+                <input type="submit" class="numbtn" name="num"value="8">
+                <input type="submit" class="numbtn" name="num"value="9">
+                <input type="submit" class="calbtn" name="op"value="+"> <br><br>
+                <input type="submit" class="numbtn" name="num"value="4">
+                <input type="submit" class="numbtn" name="num"value="5">
+                <input type="submit" class="numbtn" name="num"value="6">
+                <input type="submit" class="calbtn" name="op"value="-"><br><br>
+                <input type="submit" class="numbtn" name="num"value="1">
+                <input type="submit" class="numbtn" name="num"value="2">
+                <input type="submit" class="numbtn" name="num"value="3">
+                <input type="submit" class="calbtn" name="op"value="*"><br><br>
+                <input type="submit" class="c" name="num"value="c">
+                <input type="submit" class="numbtn" name="num"value="0">
+                <input type="submit" class="equal" name="equal"value="=">
+                <input type="submit" class="calbtn" name="op"value="/">
 
-    </form>
+
+            </form>
+        </div>
+
+
+
 </body>
 </html>
